8581f09a3358fd93904b761cd65fff456863bddb
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-04-12 20:56:00 +0100

    Refactor json response as dict

--- main.py
+++ main.py
@@ -35,24 +35,24 @@ def home():
 # Get a random café from the database
 @app.route("/random", methods=["GET", "POST"])
 def random_cafe():
-
     if request.method == "GET":
-
         cafes = db.session.query(Cafe).all()
-        random_cafe = random.choice(cafes)
+        cafe_choice = random.choice(cafes)
 
-        return jsonify(name=random_cafe.name,
-                       map_url=random_cafe.map_url,
-                       img_url=random_cafe.img_url,
-                       location=random_cafe.location,
-                       has_sockets=random_cafe.has_sockets,
-                       has_toilets=random_cafe.has_toilet,
-                       has_wifi=random_cafe.has_wifi,
-                       can_take_calls=random_cafe.can_take_calls,
-                       seats=random_cafe.seats,
-                       coffee_price=random_cafe.coffee_price)
+        return jsonify(cafe={"name": cafe_choice.name,
+                             "map_url": cafe_choice.map_url,
+                             "img_url": cafe_choice.img_url,
+                             "location": cafe_choice.location,
+                             "has_sockets": cafe_choice.has_sockets,
+                             "has_toilets": cafe_choice.has_toilet,
+                             "has_wifi": cafe_choice.has_wifi,
+                             "can_take_calls": cafe_choice.can_take_calls,
+                             "seats": cafe_choice.seats,
+                             "coffee_price": cafe_choice.coffee_price
+                             })
 
     pass
+
 
 ## HTTP GET - Read Record
 
